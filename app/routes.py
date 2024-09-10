5c5470ece9711f39dac1aed2b1cdc239d77990fd
cyrusb01 <cyrusb01@outlook.com>
2024-09-09 19:16:35 -0500

    updating home page

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import render_template
 
 from app import app
-from app.scrape import get_coinbase_assets, get_eth_assets, get_sol_assets
+# from app.scrape import get_coinbase_assets, get_eth_assets, get_sol_assets
 
 
 @app.route("/")
@@ -18,44 +18,45 @@ def projects():
 
 @app.route("/portfolio", methods=["GET", "POST"])
 def portfolio():
+    return render_template("projects.html")
 
-    # Fetch data once a day from apis and cache it
-    today = datetime.now().strftime("%Y/%m/%d")
-    try:
-        sol_tokens = get_sol_assets(today)  # Returns list
-    except:
-        sol_tokens = []
-    try:
-        eth_tokens = get_eth_assets(today)  # Returns dict
-    except:
-        eth_tokens = {}
-    try:
+#     # Fetch data once a day from apis and cache it
+#     today = datetime.now().strftime("%Y/%m/%d")
+#     try:
+#         sol_tokens = get_sol_assets(today)  # Returns list
+#     except:
+#         sol_tokens = []
+#     try:
+#         eth_tokens = get_eth_assets(today)  # Returns dict
+#     except:
+#         eth_tokens = {}
+#     try:
 
-        coinbase_tokens = get_coinbase_assets(today)  # Returns dict
-    except:
-        coinbase_tokens = {}
-    # Sorts coinbase Tokens by value
+#         coinbase_tokens = get_coinbase_assets(today)  # Returns dict
+#     except:
+#         coinbase_tokens = {}
+#     # Sorts coinbase Tokens by value
 
-    eth_tickers = list(eth_tokens.keys())
-    coinbase_tickers = [token["asset"] for token in coinbase_tokens]
-    sol_tickers = sol_tokens
+#     eth_tickers = list(eth_tokens.keys())
+#     coinbase_tickers = [token["asset"] for token in coinbase_tokens]
+#     sol_tickers = sol_tokens
 
-    pie_data = [
-        {"name": coinbase_tokens["asset"], "y": coinbase_tokens["allocation"]}
-        for coinbase_tokens in coinbase_tokens
-    ]
-    print(pie_data)
+#     pie_data = [
+#         {"name": coinbase_tokens["asset"], "y": coinbase_tokens["allocation"]}
+#         for coinbase_tokens in coinbase_tokens
+#     ]
+#     print(pie_data)
 
-    return render_template(
-        "portfolio.html",
-        pie_data=pie_data,
-        eth_l=len(eth_tickers),
-        eth_tickers=eth_tickers,
-        sol_l=len(sol_tickers),
-        sol_tickers=sol_tickers,
-        cb_l=len(coinbase_tickers),
-        cb_tickers=coinbase_tickers,
-    )
+#     return render_template(
+#         "portfolio.html",
+#         pie_data=pie_data,
+#         eth_l=len(eth_tickers),
+#         eth_tickers=eth_tickers,
+#         sol_l=len(sol_tickers),
+#         sol_tickers=sol_tickers,
+#         cb_l=len(coinbase_tickers),
+#         cb_tickers=coinbase_tickers,
+#     )
 
 
 @app.route("/trading")
